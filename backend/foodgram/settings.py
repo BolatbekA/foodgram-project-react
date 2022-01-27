1b5daa26d58b9029f55fc4f3c3654330715196e6
Bolatbek <abdukerimovbn@yandex.ru>
2022-01-27 13:31:53 +0300

    fix after review

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,22 +1,12 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+SECRET_KEY = os.environ.get('SECRET_KEY')  # 'qs(7gned^_58+3w)gq$00iummsipdkvvmp#=tc#aw3-6&ygyej'
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')  # 'qs(7gned^_58+3w)gq$00iummsipdkvvmp#=tc#aw3-6&ygyej'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
