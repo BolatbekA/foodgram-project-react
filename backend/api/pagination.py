37dab0f5e8ef7ebe55c772fcd803093cba3b0a05
Bolatbek <abdukerimovbn@yandex.ru>
2022-01-27 19:53:30 +0300

    fix after review

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -7,4 +7,4 @@ class ApiPagination(PageNumberPagination):
 
 class CustomUserPagination(PageNumberPagination):
     page_size = 1
-    page_size_query_param = "limit"
+    page_size_query_param = 'limit'
